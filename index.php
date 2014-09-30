3662cf0db82b420f5746dec2110da11dbc256e19
Brad Bodily <bod12002@byui.edu>
2014-09-30 12:06:20 -0600

    First commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>2.02 Team Readiness Project</h1>
+        <h2>CS 313</h2>
+        <form>
+            Name: <input><br>
+            Email: <input><br>
+            <label>Major</label><br>
+            <input type="radio">Computer Science<br>
+            <input type="radio">Web Development and Design<br>
+            <input type="radio">Computer information Technology<br>
+            <input type="radio">Computer Engineering<br>
+            <label>Places you have visited</label><br>
+            <input type="checkbox">North America<br>
+            <input type="checkbox">South America<br>
+            <input type="checkbox">Europe<br>
+            <input type="checkbox">Asia<br>
+            <input type="checkbox">Australia<br>
+            <input type="checkbox">Africa<br>
+            <input type="checkbox">Antartica<br>
+            Comments: <textarea></textarea>
+        </form>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
