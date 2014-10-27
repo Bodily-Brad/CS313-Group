df1ca8b0a63b72e06187a1d5562ea7de3894c108
Brad Bodily <bod12002@byui.edu>
2014-10-27 09:56:07 -0600

    Added basic structure for 6.02 assignment

---
+++ 6.02/index.php
@@ -0,0 +1,89 @@
+<?php
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// Session support
+session_start();
+
+// Connect to database
+require_once('dbConnection.php');
+
+// Get Action
+$action = getVariable("action");
+
+switch (strtolower($action))
+{
+    case "createuser":
+        // TO DO: Add code to create new user
+        
+        // IF SUCCESSFUL, show welcome page
+        if (true)   // For now, just assume it worked
+        {
+            // Show welcome page
+            include('views/welcomePage.php');            
+        }
+        break;
+    case "signin":
+        // Check if credentials are valid
+        if (getCredentialsAreValid($username, $passwordHash))
+        {
+            // Show welcome page
+            include('views/welcomePage.php');
+        }
+        // Otherwise, show login form
+        else
+        {
+            include('views/loginForm.php');            
+        }
+        break;
+    default:
+        // Check if user is already logged in
+        if (getUserIsLoggedIn())
+        {
+            // Show welcome page
+            include('views/welcomePage.php');
+        }
+        // Otherwise, show login form
+        else
+        {
+            include('views/loginForm.php');
+        }
+        break;
+}
+    
+
+// TO DO: Create a function that returns true if the specified username and
+// password hash are valid; otherwise, false;
+function getCredentialsAreValid($username, $passwordHash)
+{
+    // For now, just return true
+    return true;
+}
+
+// TO DO: Create a function that returns true if a valid user is logged in;
+// otherwise, false.
+function getUserIsLoggedIn()
+{
+    // For now, just return false
+    return false;
+}
+
+// Gets a passed variable
+function getVariable($variableName)
+{
+    if (isset($_GET[$variableName])) {
+        $return = $_GET[$variableName];
+    } elseif (isset($_POST[$variableName])) {
+        $return = $_POST[$variableName];
+    } else {
+        return NULL;
+    }  
+
+    return $return;
+}
+
+
+?>
